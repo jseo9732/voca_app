275782b47cf3f2012652859665cc48c60a97ef64
jseo9732 <79249376+jseo9732@users.noreply.github.com>
2021-05-14 18:07:25 +0900

    Add files via upload

---
+++ src/component/Word.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+function Word({ word }) {
+    const [isShow,setIsShow] = useState(false);
+    const [isDone,setIsDone] = useState(word.isDone);
+    
+
+    function toggleShow(){
+        setIsShow(!isShow);
+    }
+
+    function toggleDone(){
+        fetch(`http://localhost:3001/words/${word.id}`, {
+            method: "PUT",
+            headers: {
+                "content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                ...word,
+                isDone: !isDone
+            })
+        }).then(res => {
+            console.log(res);
+            if(res.ok) {
+                setIsDone(!isDone);
+            }
+        });
+    }
+
+    function del() {
+        if (window.confirm("삭제하시겠습니까?")) {
+            fetch(`http://localhost:3001/words/${word.id}`, {
+                method: "DELETE"
+            });
+        }
+    }
+
+    return(
+        <tr className={isDone ? 'off' : ""}>
+            <td>
+                <input type="checkbox" checked={isDone} onChange={toggleDone} />
+            </td>
+            <td>{word.eng}</td>
+            <td>{isShow && word.kor}</td>
+            <td>
+                <button onClick={ toggleShow }>뜻 {isShow ? '숨기기' :'보이기'}</button>
+                <button className="btn_del" onClick={ del }>삭제</button>
+            </td>
+        </tr>
+    )
+}
+
+export default Word;
